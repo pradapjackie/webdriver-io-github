cf2d97e057f214e03d7d072e115dc6066fe8f66e
Pradap Pandiyan <pradapjackie@gmail.com>
2024-03-26 21:53:00 +0400

    added wdio

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -62,7 +62,7 @@ export const config: Options.Testrunner = {
         maxInstances: 1,
         browserName: 'chrome',
         'goog:chromeOptions': {
-            args: ["--headless", "user-agent=...","--disable-gpu","--window-size=1440,735"]
+            args: ['headless', 'disable-gpu']
         }
 
     } ],
